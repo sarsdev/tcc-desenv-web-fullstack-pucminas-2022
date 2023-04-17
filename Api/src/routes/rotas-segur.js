ab662124f92913c324acb2787c475a340ec317d2
Renan Sá <sarsdesenv@gmail.com>
2023-04-17 20:05:28 -0300

    Organizando o repositório
    - Removendo a API do repositório pois ela foi movida para outro
    - Removendo a pasta App, pois tinha muitos níveis de pastas

--- Api/src/routes/rotas-segur.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const Controlador = require('../controller/controlador-segur')
-
-const router = express.Router()
-
-router.post('/token', Controlador.validaDados, Controlador.postToken)
-
-exports.rotasSegur = router
