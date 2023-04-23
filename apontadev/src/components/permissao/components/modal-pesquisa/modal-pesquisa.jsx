c3166b82874bc0dfcfba3452a73184273e837c8e
Renan Sá <sarsdesenv@gmail.com>
2023-04-23 14:46:34 -0300

    Ajustada a tela de cadastrado de permissão
    - Finalizada a modal de pesquisa
    - Ajustados os campos Equipes, Funções e Usuários que utilizam a modal de pesquisa

--- apontadev/src/components/permissao/components/modal-pesquisa/modal-pesquisa.jsx
+++ apontadev/src/components/permissao/components/modal-pesquisa/modal-pesquisa.jsx
@@ -1,3 +1,4 @@
+import './modal-pesquisa.css'
 import React, { useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
@@ -9,11 +10,20 @@ import Pagination from 'react-bootstrap/Pagination'
 import { ServicoPermissao } from '../../../../service/servico'
 
 function ModalPesquisa(props) {
+    const qtdLinhasPaginacao = 5
+
     const [modalCodigo, setModalCodigo] = useState('')
+    const [codigoInvalido, setCodigoInvalido] = useState(false)
     const [modalDescricao, setModalDescricao] = useState('')
+    const [descricaoInvalida, setDescricaoInvalida] = useState(false)
     const [dadosTabela, setDadosTabela] = useState([])
+    const [linhasMarcadas, setLinhasMarcadas] = useState([])
+    const [totalPaginas, setTotalPaginas] = useState(1)
+    const [paginaAtual, setPaginaAtual] = useState(1)
 
     useEffect(() => {
+        setLinhasMarcadas([...props.selecionados])
+        setPaginaAtual(1)
         let dadosLogin = {
             usuario: props.usuario.email,
             senha: props.usuario.dados_acesso.senha
@@ -26,7 +36,11 @@ function ModalPesquisa(props) {
                         console.error(resp.msgErro)
                         setDadosTabela([])
                     } else {
-                        let dados = resp.dados.map((v) => { return { codigo: v._id, descricao: v.nome } })
+                        console.log(props.selecionados)
+                        let dados = resp.dados.map((v) => {
+                            let indMarcado = props.selecionados.filter((valor, i, o) => valor.codigo === v._id).length > 0
+                            return { marcado: indMarcado, codigo: v._id, descricao: v.nome } 
+                        })
                         setDadosTabela(dados)
                     }
                 }).catch((err) => {
@@ -41,7 +55,10 @@ function ModalPesquisa(props) {
                         console.error(resp.msgErro)
                         setDadosTabela([])
                     } else {
-                        let dados = resp.dados.map((v) => { return { codigo: v._id, descricao: v.nome } })
+                        let dados = resp.dados.map((v) => { 
+                            let indMarcado = props.selecionados.filter((valor, i, o) => valor.codigo === v._id).length > 0
+                            return { marcado: indMarcado, codigo: v._id, descricao: v.nome } 
+                        })
                         setDadosTabela(dados)
                     }
                 }).catch((err) => {
@@ -56,7 +73,10 @@ function ModalPesquisa(props) {
                         console.error(resp.msgErro)
                         setDadosTabela([])
                     } else {
-                        let dados = resp.dados.map((v) => { return { codigo: v._id, descricao: v.dados_pessoais.nome } })
+                        let dados = resp.dados.map((v) => { 
+                            let indMarcado = props.selecionados.filter((valor, i, o) => valor.codigo === v._id).length > 0
+                            return { marcado: indMarcado, codigo: v._id, descricao: v.dados_pessoais.nome } 
+                        })
                         setDadosTabela(dados)
                     }
                 }).catch((err) => {
@@ -70,8 +90,19 @@ function ModalPesquisa(props) {
         }
     }, [props.titulo])
 
+    function valorAlfanumerico(str, campo){
+        switch (campo) {
+            case 'codigo':
+                return /^\w+$/.test(str)
+            case 'descricao':
+                return /^[\w\s]+$/.test(str)
+            default:
+                return false
+        }
+    }
+
     function RetornarSelecionados() {
-        props.onHide()
+        props.onHide({ campo: props.titulo, selecionados: linhasMarcadas })
     }
 
     function ValidaCampoModal(e) {       
@@ -90,7 +121,135 @@ function ModalPesquisa(props) {
     }
 
     function InsereLinhasTabela(dados) {
-        return dados.map((v, i, o) => <tr key={i}><td>{v.codigo}</td><td>{v.descricao}</td></tr>)
+        let dadosFiltrados = dados
+        if(modalCodigo) {
+            if(valorAlfanumerico(modalCodigo, 'codigo')) {
+                if(codigoInvalido) {
+                    setCodigoInvalido(false)
+                }
+                dadosFiltrados = dadosFiltrados.filter((v, i, o) => v.codigo.indexOf(modalCodigo) > -1)
+            } else {
+                if(!codigoInvalido) {
+                    setCodigoInvalido(true)
+                }
+            }
+        }
+        if(modalDescricao) {
+            if(valorAlfanumerico(modalDescricao, 'descricao')) {
+                if(descricaoInvalida) {
+                    setDescricaoInvalida(false)
+                }
+                dadosFiltrados = dadosFiltrados.filter((v, i, o) => v.descricao.indexOf(modalDescricao) > -1)
+            } else {
+                if(!descricaoInvalida) {
+                    setDescricaoInvalida(true)
+                }
+            }
+        }
+        if(dadosFiltrados.length > qtdLinhasPaginacao) {
+            let qtdPaginas = Math.ceil(dadosFiltrados.length / qtdLinhasPaginacao)
+            if(totalPaginas !== qtdPaginas) {
+                setTotalPaginas(qtdPaginas)
+                if(paginaAtual > qtdPaginas) {
+                    setPaginaAtual(qtdPaginas)
+                }
+            }
+            dadosFiltrados = dadosFiltrados.filter((v, i, o) => i >= ((qtdLinhasPaginacao * paginaAtual) - qtdLinhasPaginacao) && i <= (qtdLinhasPaginacao * paginaAtual)-1)
+        } else {
+            if(totalPaginas !== 1) {
+                setTotalPaginas(1)
+                if(paginaAtual > 1) {
+                    setPaginaAtual(1)
+                }
+            }
+        }
+        return dadosFiltrados.map((v, i, o) => 
+            <tr key={i}>
+                <td>
+                    <Form.Check
+                        id={'check'+i}
+                        type='checkbox'
+                        label=''
+                        value={JSON.stringify(v)}
+                        checked={v.marcado}
+                        onChange={(e) => SelecionarLinha(e)} />
+                </td>
+                <td>
+                    {v.codigo}
+                </td>
+                <td>
+                    {v.descricao}
+                </td>
+            </tr>)
+    }
+
+    function MontaPaginacaoTabelaModal() {
+        if(totalPaginas >= 1 && totalPaginas <= 3) {
+            let listaPaginas = []
+            for (let index = 1; index <= totalPaginas; index++) {
+                listaPaginas.push(index)                                        
+            }
+            return (<>
+                <Pagination.First
+                    id='first'
+                    hidden={true}
+                    onClick={(e) => MudaPaginaTabela(e)} />
+                {listaPaginas.map((v, i, o) => <Pagination.Item
+                                                    id={'pag'+v}
+                                                    key={i} 
+                                                    className={paginaAtual===v ? 'destaquePag' : ''}
+                                                    onClick={(e) => MudaPaginaTabela(e)} >
+                                                    {v}
+                                                </Pagination.Item>)}
+                <Pagination.Last
+                    id='last'
+                    hidden={true}
+                    onClick={(e) => MudaPaginaTabela(e)} />
+            </>)
+        } else {
+            return <Pagination.Item>{0}</Pagination.Item>
+        }
+    }
+
+    function MudaPaginaTabela(e) {
+        let campo = e.target.id
+        let valor = e.target.text        
+        switch (campo) {
+            case 'first':
+                if(paginaAtual !== 1) {
+                    setPaginaAtual(1)
+                }
+                break
+            case 'last':
+                if(paginaAtual !== totalPaginas) {
+                    setPaginaAtual(totalPaginas)
+                }
+                break
+            default:
+                if(campo.length > 3 && campo.substring(0, 3) === 'pag') {
+                    let valorNumerico = +valor
+                    if(paginaAtual !== valorNumerico) {
+                        setPaginaAtual(valorNumerico)
+                    }
+                }
+                break
+        }
+    }
+
+    function SelecionarLinha(e) {        
+        let linhas
+        let item = JSON.parse(e.target.value)
+        if(e.target.checked) {
+            linhas = linhasMarcadas
+            linhas.push(JSON.parse(e.target.value))
+            setLinhasMarcadas(linhas)
+        } else {
+            let linhas = linhasMarcadas.filter((v, i, o) => v.codigo !== item.codigo)
+            setLinhasMarcadas(linhas)
+        }
+        let indiceDado = dadosTabela.findIndex((v, i, o) => v.codigo === item.codigo)
+        dadosTabela[indiceDado].marcado = e.target.checked
+        setDadosTabela([...dadosTabela])        
     }
 
     return (
@@ -110,6 +269,7 @@ function ModalPesquisa(props) {
                             type='text'
                             value={modalCodigo}
                             placeholder='Código...'
+                            isInvalid={codigoInvalido}
                             onChange={(evento) => ValidaCampoModal(evento)} />
                     </Col>
                     <Col  md='9'>
@@ -118,6 +278,7 @@ function ModalPesquisa(props) {
                             type='text'
                             value={modalDescricao}
                             placeholder='Descrição...'
+                            isInvalid={descricaoInvalida}
                             onChange={(evento) => ValidaCampoModal(evento)} />
                     </Col>
                 </Row>
@@ -126,6 +287,7 @@ function ModalPesquisa(props) {
                         <Table striped>
                             <thead>
                                 <tr>
+                                    <th>*</th>
                                     <th>Código</th>
                                     <th>Descrição</th>
                                 </tr>
@@ -134,10 +296,8 @@ function ModalPesquisa(props) {
                                 {InsereLinhasTabela(dadosTabela)}
                             </tbody>
                         </Table>
-                        <Pagination className='d-flex justify-content-center'>
-                            <Pagination.First />
-                            <Pagination.Item>{1}</Pagination.Item>
-                            <Pagination.Last />
+                        <Pagination className='d-flex justify-content-center'>                            
+                            {MontaPaginacaoTabelaModal()}                            
                         </Pagination>
                     </Col>
                 </Row>
@@ -145,7 +305,7 @@ function ModalPesquisa(props) {
             <Modal.Footer>
                 <Button
                     onClick={() => RetornarSelecionados()} >
-                    Salvar
+                    Retornar
                 </Button>
             </Modal.Footer>
         </Modal>
--- apontadev/src/components/permissao/components/permissao-manutencao/permissao-manutencao.jsx
+++ apontadev/src/components/permissao/components/permissao-manutencao/permissao-manutencao.jsx
@@ -1,5 +1,5 @@
 import './permissao-manutencao.css'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -14,8 +14,44 @@ import ModalPesquisa from '../modal-pesquisa/modal-pesquisa'
 
 function PermissaoManutencao({usuariologin}) {
     const [tipoPermissao, setTipoPermissao] = useState('cadastrado')
+    const [equipes, setEquipes] = useState('')
+    const [equipesSelec, setEquipesSelec] = useState([])
+    const [funcoes, setFuncoes] = useState('')
+    const [funcoesSelec, setFuncoesSelec] = useState([])
+    const [usuarios, setUsuarios] = useState('')
+    const [usuariosSelec, setUsuariosSelec] = useState([])
     const [tituloModal, setTituloModal] = useState('')
     const [mostrarModalPesquisa, setMostrarModalPesquisa] = useState(false)
+
+    useEffect(() => {
+        if(equipesSelec.length === 1) {
+            setEquipes(equipesSelec[0].descricao)
+        } else if(equipesSelec.length >= 1) {
+            setEquipes(equipesSelec[0].descricao + ' ... (+' + (equipesSelec.length-1) + ')')
+        } else {
+            setEquipes('')
+        }
+    }, [equipesSelec])
+
+    useEffect(() => {
+        if(funcoesSelec.length === 1) {
+            setFuncoes(funcoesSelec[0].descricao)
+        } else if(funcoesSelec.length >= 1) {
+            setFuncoes(funcoesSelec[0].descricao + ' ... (+' + (funcoesSelec.length-1) + ')')
+        } else {
+            setFuncoes('')
+        }      
+    }, [funcoesSelec])
+
+    useEffect(() => {
+        if(usuariosSelec.length === 1) {
+            setUsuarios(usuariosSelec[0].descricao)
+        } else if(usuariosSelec.length >= 1) {
+            setUsuarios(usuariosSelec[0].descricao + ' ... (+' + (usuariosSelec.length-1) + ')')
+        } else {
+            setUsuarios('')
+        }     
+    }, [usuariosSelec])
 
     function AbreModalPesquisa(pesquisarPor) {
         switch (pesquisarPor) {
@@ -30,6 +66,26 @@ function PermissaoManutencao({usuariologin}) {
             case 'botao_usuarios':
                 setTituloModal('Usuários')
                 setMostrarModalPesquisa(!mostrarModalPesquisa)
+                break
+            default:
+                break
+        }
+    }
+
+    function RetornaDadosModal(dados) {
+        setMostrarModalPesquisa(false)
+        if(!(dados && dados.campo)) {
+            return
+        }
+        switch(dados.campo) {
+            case 'Equipes':
+                setEquipesSelec([...dados.selecionados])
+                break
+            case 'Funções':
+                setFuncoesSelec([...dados.selecionados])
+                break
+            case 'Usuários':
+                setUsuariosSelec([...dados.selecionados])
                 break
             default:
                 break
@@ -73,7 +129,8 @@ function PermissaoManutencao({usuariologin}) {
                             <Form.Control
                                 id='equipes'
                                 disabled={true}
-                                placeholder="Escolha a equipe..." />
+                                placeholder="Escolha a equipe..."
+                                value={equipes} />
                             <Button 
                                 id="botao_equipes"
                                 variant="outline-secondary"
@@ -85,7 +142,8 @@ function PermissaoManutencao({usuariologin}) {
                             <Form.Control
                                 id='funcoes'
                                 disabled={true}
-                                placeholder="Escolha a função..." />
+                                placeholder="Escolha a função..."
+                                value={funcoes} />
                             <Button 
                                 id="botao_funcoes" 
                                 variant="outline-secondary"
@@ -97,7 +155,8 @@ function PermissaoManutencao({usuariologin}) {
                             <Form.Control
                                 id='usuarios'
                                 disabled={true}
-                                placeholder="Escolha o usuário..." />
+                                placeholder="Escolha o usuário..."
+                                value={usuarios} />
                             <Button 
                                 id="botao_usuarios" 
                                 variant="outline-secondary"
@@ -186,8 +245,9 @@ function PermissaoManutencao({usuariologin}) {
             <ModalPesquisa
                 usuario={usuariologin}
                 titulo={tituloModal}
+                selecionados={ tituloModal==='Equipes' ? equipesSelec : tituloModal==='Funções' ? funcoesSelec : usuariosSelec }
                 show={mostrarModalPesquisa}
-                onHide={() => setMostrarModalPesquisa(false)}
+                onHide={(dadosSelecionados) => RetornaDadosModal(dadosSelecionados)}
             />
         </Container>
     )
